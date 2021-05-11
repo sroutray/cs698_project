20ea29fa221ebe2896812c5d90e6a473c6239d78
sroutray <sandeeproutray1999@gmail.com>
2021-05-11 04:38:58 -0400

    M2 model

---
+++ train_m2.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import utils
+
+
+
+def train(  x_labelled, y, x_unlabelled,
+        x_valid, y_valid,
+        epochs, 
+        learning_rate=3e-4, 
+        stop_iter=100):
+
+    pass
